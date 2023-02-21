dbe5ddf0a6a18c04e32acc51f448172ed63679d7
GHI-Peace <hunugarbaiyabo@gmail.com>
2023-02-21 05:22:21 -0800

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Make alphabet x10 1 Line
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+
+{
+	char c;
+	int i = 0;
+
+	while (i <= 0)
+	{
+	for (c - 'a'; c <= 'a'; c++)
+	{
+	_putchar('\n');
+	1++;
+	}
+}
