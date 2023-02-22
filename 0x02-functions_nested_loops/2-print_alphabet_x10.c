66098429f7091ad65a944e88f30fd119d142244d
GHI-Peace <hunugarbaiyabo@gmail.com>
2023-02-22 01:04:19 -0800

    task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,20 +2,20 @@
 
 /**
  * print_alphabet_x10 - Function that print 10 times the alphabet a..z
- * 
+ *
  * Return: Always 0 (Success)
  */
 
 void print_alphabet_x10(void)
 {
-       char c, i;
+	char c, i;
 
-        for (i = 0; i <= 9; i++)
-        {
-                for (c = 'a'; c <= 'z'; c++)
-                {
-                        _putchar(c);
-                }
-                _putchar('\n');
-        }
+	for (i = 0; i <= 9; i++)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+	}
 }
