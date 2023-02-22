6de6631299477f97b85762f5993a47ed8e56d52c
GHI-Peace <hunugarbaiyabo@gmail.com>
2023-02-22 04:20:32 -0800

    task 10

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,33 +1,17 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
- * main - computes and prints the sum of all the multiples
- * of 3 or 5 below 1024
- * Return: Always 0 (Success)
+ * add - adds two integers and returns the result
+ * @a: int to be added in b
+ * @b: int to be added to a
+ * Return: sum of a and b
  */
 
-int main(void)
+int add(int a, int b)
 {
-	unsigned long int sum3, sum5, sum;
-	int i;
+	int c;
 
-	sum3 = 0;
-	sum5 = 0;
-	sum = 0;
+	c = a + b;
 
-	for (i = 0; i < 1024; ++i)
-	{
-	if ((i % 3) == 0)
-	{
-	sum3 = sum3 + i;
-	}
-
-	else if ((i % 5) == 0)
-	{
-	sum5 = sum5 + i;
-	}
-	}
-	sum = sum3 + sum5;
-	printf("%lu\n", sum);
-	return (0);
+	return (c);
 }
